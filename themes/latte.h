97e138892e32d02dac882019615674457d2ee233
IKISUGI-Tadokoro <yhamada2005@gmail.com>
2026-05-15 22:26:49 +0900

    Made accent color pink

--- themes/latte.h
+++ themes/latte.h
@@ -2,4 +2,4 @@ static const char col_gray1[] = "#dce0e8";
 static const char col_gray2[] = "#ccd0da";
 static const char col_gray3[] = "#5c5f77";
 static const char col_gray4[] = "#4c4f69";
-static const char col_accent[] = "#cba6f7";
+static const char col_accent[] = "#dc8a78";
