76254897c1e55ba8b88a8cd4c96f670356d77631
IKISUGI-Tadokoro <yhamada2005@gmail.com>
2026-05-15 22:19:20 +0900

    Added catppuccin themes

---
+++ themes/latte.h
@@ -0,0 +1,5 @@
+static const char col_gray1[] = "#dce0e8";
+static const char col_gray2[] = "#ccd0da";
+static const char col_gray3[] = "#5c5f77";
+static const char col_gray4[] = "#4c4f69";
+static const char col_accent[] = "#cba6f7";
---
+++ themes/mocha.h
@@ -0,0 +1,5 @@
+static const char col_gray1[] = "#1e1e2e";
+static const char col_gray2[] = "#313244";
+static const char col_gray3[] = "#cdd6f4";
+static const char col_gray4[] = "#bac2de";
+static const char col_accent[] = "#8839ef";
